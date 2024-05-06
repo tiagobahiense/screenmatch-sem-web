95fdcaa52e31d591fab77a7c97dd189d85391bf7
Tiago Bahiense <tgbahiense@gmail.com>
2024-05-06 16:25:00 -0300

    modelando a busca por episodios utilizando Lambda

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
@@ -11,6 +12,7 @@ import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
+
     private ConsumoAPI consumo = new ConsumoAPI();
 
     private ConverteDados conversor = new ConverteDados();
@@ -32,5 +34,14 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
