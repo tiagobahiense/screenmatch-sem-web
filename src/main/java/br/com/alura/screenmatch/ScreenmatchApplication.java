67400c0799eb70720ef8c7d2b86e37950f3251ba
Tiago Bahiense <tgbahiense@gmail.com>
2024-05-10 15:51:59 -0300

    modelando categorias e series

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
